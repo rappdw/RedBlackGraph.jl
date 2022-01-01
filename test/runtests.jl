04c3b2b7a172c9c35ff83b46611ef2deb50736d6
Dan Rapp <rappdw@gmail.com>
2022-01-01 09:21:02 -0700

    Add tests to demonstrate linear algebra

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,4 @@ using SafeTestsets
 @safetestset "Basic Avus Tests" begin include("avus_tests.jl") end
 @safetestset "Shortest Paths Tests" begin include("shortestpaths_test.jl") end
 @safetestset "Relationship Tests" begin include("relationship_test.jl") end
+@safetestset "Avus Linear Algebra Tests" begin include("linalg_test.jl") end
